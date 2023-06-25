c5a52b3276adfdaa2c46255aa26d050bef4ed53b
Cyo90 <moragasferrera@gmail.com>
2023-06-25 18:41:54 +0200

    Initial commit
    Initial commit

---
+++ kubejs/startup_scripts/script.js
@@ -0,0 +1,141 @@
+SkillEvents.registration(event => {
+
+  /* FREEMODE */
+
+  event.add('freemode', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+  /* Edad de madera */
+  event.add('edad_madera', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('madera_pico', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('madera_hacha', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('madera_azada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('madera_pala', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('madera_espada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+
+  /***********************************************/
+
+    /* Edad de piedra */
+  event.add('edad_piedra', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('piedra_pico', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('piedra_hacha', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('piedra_azada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('piedra_pala', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('piedra_espada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+    /* Edad de hierro */
+  event.add('edad_hierro', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_pico', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_hacha', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_azada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_pala', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_espada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('hierro_escudo', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+  /* Edad de oro */
+  event.add('edad_oro', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+    /* Edad de diamante */
+  event.add('edad_diamante', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_pico', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_hacha', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_azada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_pala', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_espada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_mesa_encantamiento', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('diamante_libros_encantamiento', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+    /* Edad de netherita */
+  event.add('edad_netherita', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_smith', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_pico', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_hacha', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_azada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_pala', 'basic', skill => {
+    skill.initialValue(false);
+  });
+  event.add('netherita_espada', 'basic', skill => {
+    skill.initialValue(false);
+  });
+
+  /***********************************************/
+
+});
